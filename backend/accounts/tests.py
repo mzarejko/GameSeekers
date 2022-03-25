df92e8a8cbe504461b646e239d81c882ec317ead
mzarejko <michalzarejko@gmail.com>
2022-03-25 13:11:11 +0100

    [CI/CD] testy, workflow

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+class UserTestCase(TestCase):
+    def SetUp(self):
+        user = User.objects.create(username="chuck noris",
+                            email="jfkwf@efe.com")
+        user.set_password('111111')
+        user.save()
+        print(user)
+
+
+    def test_create_user(self):
+        user = User.objects.get(username="chuck noris")
+        self.assertNotEqual(user, None)
